213034e2996e2deb93c3dfacf0b808cb83c4a280
pujaraj618 <126242695+pujaraj618@users.noreply.github.com>
2023-02-28 21:40:41 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import PlayPause from './PlayPause'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <PlayPause/>
+      </div>
+    )
+  }
+}
---
+++ src/PlayPause.jsx
@@ -0,0 +1,27 @@
+import React, { useRef, useState } from 'react'
+import VIDEO from './videoplayback.mp4'
+
+const PlayPause = () => {
+    let [video , setVideo]=useState(VIDEO)
+    let [play , setPlay]=useState(false);
+    let videoRef=useRef()
+    let handleChange=()=>{
+        if(!play)
+        {
+            setPlay(true)
+            videoRef.current.play()
+            
+        }
+        else
+        {
+            setPlay(false)
+            videoRef.current.pause()
+        }
+    }
+  return (
+    <div>
+        <video src={video}ref={videoRef}onClick={handleChange}></video>
+    </div>
+  )
+}
+export default PlayPause
